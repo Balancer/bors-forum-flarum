c31453b329efb1041844cbb03ae471855b4035c1
Balancer <balancer@balancer.ru>
2016-11-27 16:00:00 +0300

    ++

--- src/ObjectDb.php
+++ src/ObjectDb.php
@@ -5,5 +5,5 @@ namespace B2\Flarum;
 class ObjectDb extends \bors_object_db
 {
 	function storage_engine() { return \bors_storage_mysql::class; }
-	function db_name() { return \B2\Cfg::get('flarum_db'); }
+	function db_name() { return \B2\Cfg::get('flarum.db'); }
 }
--- src/Post.php
+++ src/Post.php
@@ -14,7 +14,7 @@ class Post extends ObjectDb
 			'number',
 			'create_time' => 'UNIX_TIMESTAMP(`time`)',
 			'create_datetime' => 'time',
-			'owner_id' => 'user_id',
+			'author_id' => 'user_id',
 			'type',
 			'text' => ['name' => 'content', 'type' => 'markdown'],
 			'modify_time' => 'edit_time',
@@ -60,5 +60,90 @@ class Post extends ObjectDb
 		$html = preg_replace("!<p><a href=\".+?\" target=\"_blank\" rel=\"nofollow noreferrer\">Источник</a></p>!", '', $html);
 
 		return $html;
+	}
+
+	static function create($data)
+	{
+		$app = App::instance();
+		$flarum_app = $app->flarum_app;
+//		$flarum_actor = Sub\User::find(popval($data, 'author_id'));
+		$flarum_actor = \Flarum\Core\User::find(popval($data, 'author_id'));
+		$flarum_discussion = \Flarum\Core\Discussion::find(popval($data, 'topic_id'));
+
+//		dump($data, $flarum_discussion);
+
+//		$flarum_actor->forced_time = true;
+
+		$flarum_data = [
+			'attributes' => [
+				'content' => $data['text'],
+//				'time' => \Carbon\Carbon::createFromTimestampUTC($data['create_time'])->toDateTimeString(),
+				'is_approved' => true,
+			],
+		];
+
+		$ipAddress = popval($data, 'author_ip');
+
+		$cmd = new \Flarum\Core\Command\PostReply($flarum_discussion->id, $flarum_actor, $flarum_data, $ipAddress);
+		$handler = new \Flarum\Core\Command\PostReplyHandler(
+			$flarum_app->events,
+			new	\Flarum\Core\Repository\DiscussionRepository,
+			new	\Flarum\Core\Notification\NotificationSyncer(new \Flarum\Core\Repository\NotificationRepository, new \Flarum\Core\Notification\NotificationMailer($flarum_app->mailer)),
+			new \Flarum\Core\Validator\PostValidator($flarum_app->validator, $flarum_app->events, $flarum_app->make(\Symfony\Component\Translation\TranslatorInterface::class))
+		);
+
+		$flarum_original_post = $handler->handle($cmd);
+
+		$flarum_b2_post = Post::load($flarum_original_post->id);
+		return $flarum_b2_post;
+	}
+
+	function edit($data)
+	{
+		$app = App::instance();
+		$flarum_app = $app->flarum_app;
+		$flarum_actor = \Flarum\Core\User::find($this->author_id());
+		$flarum_discussion = \Flarum\Core\Discussion::find(popval($data, 'topic_id'));
+
+//		dump($data, $flarum_discussion);
+
+//		$flarum_actor->forced_time = true;
+
+		$flarum_data = [
+			'attributes' => [
+				'content' => $data['text'],
+//				'time' => \Carbon\Carbon::createFromTimestampUTC($data['create_time'])->toDateTimeString(),
+				'is_approved' => true,
+			],
+		];
+
+		$ipAddress = popval($data, 'author_ip');
+
+    /**
+     * @param int $postId The ID of the post to edit.
+     * @param User $actor The user performing the action.
+     * @param array $data The attributes to update on the post.
+     */
+//    public function __construct($postId, User $actor, array $data)
+
+		$cmd = new \Flarum\Core\Command\EditPost($this->id(), $flarum_actor, $flarum_data);
+
+    /**
+     * @param Dispatcher $events
+     * @param PostRepository $posts
+     * @param PostValidator $validator
+     */
+//    public function __construct(Dispatcher $events, PostRepository $posts, PostValidator $validator)
+
+		$handler = new \Flarum\Core\Command\EditPostHandler(
+			$flarum_app->events,
+			new	\Flarum\Core\Repository\PostRepository,
+			new \Flarum\Core\Validator\PostValidator($flarum_app->validator, $flarum_app->events, $flarum_app->make(\Symfony\Component\Translation\TranslatorInterface::class))
+		);
+
+		$flarum_original_post = $handler->handle($cmd);
+
+		$flarum_b2_post = Post::load($flarum_original_post->id);
+		return $flarum_b2_post;
 	}
 }
